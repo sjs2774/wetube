6dae482300d2d850984ff4fa495286ffc2177fbd
Kyrie <kyrie@Kyrieui-MacBookPro.local>
2020-04-16 03:14:50 +0900

    2.14

---
+++ routers/globalRouter.js
@@ -0,0 +1,21 @@
+import express from "express";
+import routes from "../routes";
+import { 
+    logout, 
+    login, 
+    join 
+} from "../controllers/userController";
+import { 
+    search, 
+    home 
+} from "../controllers/videoController";
+
+const globalRouter = express.Router();
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search);
+
+export default globalRouter;
